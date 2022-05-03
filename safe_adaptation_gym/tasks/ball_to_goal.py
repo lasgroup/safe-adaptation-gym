9be1f042bd85346cf3cdc141284d0e09d309e6a8
yardenas <yardas@student.ethz.ch>
2022-05-03 12:20:50 +0200

    Make nice colors

--- safe_adaptation_gym/tasks/ball_to_goal.py
+++ safe_adaptation_gym/tasks/ball_to_goal.py
@@ -17,11 +17,6 @@ class BallToGoal(PushBox):
   def build_world_config(self, layout: dict, rs: np.random.RandomState) -> dict:
     config = GoToGoal.build_world_config(self, layout, rs)
     model = mjcf.RootElement()
-    texture = model.asset.add(
-        'texture',
-        name='ukraine',
-        file=os.path.join(c.BASE_DIR, 'textures', 'ukraine.png'))
-    material = model.asset.add('material', name='ukraine', texture=texture)
     ball = model.worldbody.add('body', name='box')
     ball.add('freejoint', name='box')
     ball.add(
@@ -34,8 +29,9 @@ class BallToGoal(PushBox):
         pos=[0, 0, self.SPHERE_RADIUS],
         user=[c.GROUP_OBJECTS],
         solref=[0.02, 0.1],
+        rgba=[1, 1, 1, 1],
         condim=6,
-        material=material,
+        material='ukraine',
         priority=1)
     box_config = {'bodies': {'box': ([ball.to_xml_string()], '')}}
     merge(config, box_config)
