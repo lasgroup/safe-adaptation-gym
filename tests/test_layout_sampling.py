4f63b497122b8e804cd7dafa868db0a4d0b875be
yardenas <yardas@student.ethz.ch>
2022-05-18 14:12:28 +0200

    Update the layout sampling test

--- tests/test_layout_sampling.py
+++ tests/test_layout_sampling.py
@@ -3,21 +3,21 @@ import pytest
 
 import matplotlib.pyplot as plt
 
-from safe_adaptation_gym import tasks
+from safe_adaptation_gym import benchmark
 from safe_adaptation_gym.mujoco_bridge import MujocoBridge
 from safe_adaptation_gym.utils import ResamplingError
 from safe_adaptation_gym.world import World
 from safe_adaptation_gym.robot import Robot
 
 
-@pytest.fixture(params=[tasks.GoToGoal(), tasks.PushBox()])
+@pytest.fixture(params=benchmark.TASKS)
 def good_world(request):
   world = World(
-      np.random.RandomState(0), request.param, Robot('xmls/doggo.xml'))
+      np.random.RandomState(0), request.param[1](), Robot('xmls/doggo.xml'))
   return world
 
 
-@pytest.fixture(params=[tasks.GoToGoal(), tasks.PushBox()])
+@pytest.fixture(params=benchmark.TASKS)
 def bad_world(request):
   config = {
       'hazards_size': 2.0,
@@ -26,16 +26,17 @@ def bad_world(request):
       'gremlins_size': 2.0
   }
   world = World(
-      np.random.RandomState(0), request.param, Robot('xmls/doggo.xml'), config)
+      np.random.RandomState(0), request.param[1](), Robot('xmls/doggo.xml'),
+      config)
   return world
 
 
 def layout_sampling(world):
-  mujoco_bridge = MujocoBridge('xmls/doggo.xml', world.sample_layout())
+  mujoco_bridge = MujocoBridge(Robot('xmls/doggo.xml'))
   fails = 0
   for _ in range(1000):
     try:
-      mujoco_bridge.reset(world.sample_layout())
+      mujoco_bridge.rebuild(world.sample_layout())
     except ResamplingError:
       fails += 1
   # Make sure that the fail rate is smaller than 0.5%
@@ -51,7 +52,8 @@ def test_layout_sampling(good_world):
 def test_layout_impossible(bad_world):
   ok = False
   try:
-    MujocoBridge(Robot('xmls/doggo.xml'), bad_world.sample_layout())
+    MujocoBridge(Robot('xmls/doggo.xml'))
+    bad_world.sample_layout()
   except ResamplingError:
     ok = True
   assert ok
@@ -62,7 +64,7 @@ def test_reset(good_world):
       Robot('xmls/doggo.xml'), good_world.sample_layout())
   plt.figure()
   plt.imshow(mujoco_bridge.physics.render(camera_id='fixedfar'))
-  mujoco_bridge.reset(good_world.sample_layout())
+  mujoco_bridge.rebuild(good_world.sample_layout())
   plt.figure()
   plt.imshow(mujoco_bridge.physics.render(camera_id='fixedfar'))
   plt.pause(4.0)
