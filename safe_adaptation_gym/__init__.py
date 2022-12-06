600ec1708768b7197687fb79056535927df3f871
Yarden <yarden.as@inf.ethz.ch>
2022-12-06 14:34:30 +0100

    Fix parameters for new API

--- safe_adaptation_gym/__init__.py
+++ safe_adaptation_gym/__init__.py
@@ -3,18 +3,19 @@ from typing import Optional, Dict
 from safe_adaptation_gym.benchmark import TASKS
 
 
-def make(task_name: Optional[str] = None,
+def make(
+    task_name: Optional[str] = None,
     seed: int = 666,
-         config: Optional[Dict] = None,
+    config: Optional[Dict] = None,
     rgb_observation: bool = False,
-         render_options: Optional[Dict] = None,
-         render_lidar_and_collision=True):
+    render_options: Optional[Dict] = None,
+    render_lidar_and_collision=True,
+):
     from safe_adaptation_gym.safe_adaptation_gym import SafeAdaptationGym
+
     env = SafeAdaptationGym(
-      config=config,
-        rgb_observation=rgb_observation,
-      render_lidars_and_collision=render_lidar_and_collision,
-      render_options=render_options)
+        rgb_observation=rgb_observation, render_options=render_options
+    )
     env.seed(seed)
     if task_name is not None:
         task = TASKS[task_name.lower()]
