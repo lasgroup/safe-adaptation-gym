9828ad5424b05b63814e3eb3000ff68f3cce6201
Yarden <yarden.as@inf.ethz.ch>
2024-07-29 18:01:52 +0200

    0.30

--- safe_adaptation_gym/tasks/press_buttons_scarce.py
+++ safe_adaptation_gym/tasks/press_buttons_scarce.py
@@ -23,7 +23,7 @@ class PressButtonsScarce(PressButtons):
         distance = np.linalg.norm(robot_pos - goal_pos)
         reward = tolerance(
             distance,
-            (0, 0.33),
+            (0, 0.30),
             margin=0.0,
             value_at_margin=0.0,
             sigmoid="linear",
