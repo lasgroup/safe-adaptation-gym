d05389d4c38ad5681a38b4ee70a70cb6bdc1e505
Yarden <yarden.as@inf.ethz.ch>
2024-09-04 13:47:01 +0200

    Buttons scarce very scarce and negative reward

--- safe_adaptation_gym/tasks/press_buttons_scarce.py
+++ safe_adaptation_gym/tasks/press_buttons_scarce.py
@@ -2,7 +2,6 @@ from typing import Tuple
 
 import numpy as np
 
-from dm_control.utils.rewards import tolerance
 from safe_adaptation_gym.tasks.press_buttons import PressButtons, State
 from safe_adaptation_gym.tasks.task import MujocoBridge
 
@@ -21,20 +20,22 @@ class PressButtonsScarce(PressButtons):
         goal_pos = mujoco_bridge.body_pos(self._goal_button)[:2]
         robot_pos = mujoco_bridge.body_pos("robot")[:2]
         distance = np.linalg.norm(robot_pos - goal_pos)
-        reward = tolerance(
-            distance,
-            (0, 0.30),
-            margin=0.0,
-            value_at_margin=0.0,
-            sigmoid="linear",
-        ) * (self._last_goal_distance - distance)
         self._last_goal_distance = distance
+        reward = 0.0
         info = {}
         if mujoco_bridge.robot_contacts([self._goal_button]):
             reward += 1.0
             info["goal_met"] = True
             self._sample_goal_button(rs, mujoco_bridge)
             self._state = State.BUTTON_CHANGE
+        elif mujoco_bridge.robot_contacts(
+            [
+                f"button{i}"
+                for i in self.NUM_BUTTONS
+                if f"button{i}" != self._goal_button
+            ]
+        ):
+            reward -= 1.0
         if self._state == State.BUTTON_CHANGE:
             if not self._goal_button_timer.done:
                 self._goal_button_timer.tick()
