b1b4249e1902b8d1034a8e34874de3c24c114ec7
Yarden <yarden.as@inf.ethz.ch>
2024-09-16 10:13:24 +0200

    Another run

--- safe_adaptation_gym/tasks/push_box_scarce.py
+++ safe_adaptation_gym/tasks/push_box_scarce.py
@@ -30,7 +30,7 @@ class PushBoxScarce(PushBox):
         box_distance = np.linalg.norm(robot_pos - box_pos)
         reward = tolerance(
             box_distance,
-            (0, self.GOAL_SIZE * 1.75),
+            (0, self.GOAL_SIZE * 1.5),
             margin=0.0,
             value_at_margin=0.0,
             sigmoid="linear",
