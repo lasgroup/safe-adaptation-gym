56721514a6782aaf048fad055aab73f7a70be0c6
Yarden <yarden.as@inf.ethz.ch>
2024-09-20 07:44:05 +0200

    Start with unsafe first

--- safe_adaptation_gym/tasks/go_to_goal_sim.py
+++ safe_adaptation_gym/tasks/go_to_goal_sim.py
@@ -10,7 +10,7 @@ class GoToGoalSim(GoToGoal):
         super(GoToGoalSim, self).__init__()
 
     def modify_tree(self, rs: RandomState, min_damping: float):
-        self._damping = np.ones((2,)) * min_damping * 50.0
+        self._damping = np.ones((2,)) * min_damping * 0.1
         return [
             (("joint", "y"), ("damping", self._damping[0])),
             (("joint", "x"), ("damping", self._damping[1])),
